1ca30ddcae89c6777c4584aa12e46674bdb02482
Servais Lucas <119533453+kalicatt@users.noreply.github.com>
2024-06-12 11:46:17 +0200

    Delete mysite directory

--- mysite/client/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Clients(models.Model):
-    id = models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=255)
-    prenom = models.CharField(max_length=255)
-

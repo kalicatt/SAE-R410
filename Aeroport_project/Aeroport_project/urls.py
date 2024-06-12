808185ed897a9cb6e54cbc0fcde9997e44dd0cdf
kalicatt <servaislucas68@gmail.com>
2024-06-13 01:05:31 +0200

    correction urls

--- Aeroport_project/Aeroport_project/urls.py
+++ Aeroport_project/Aeroport_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('API-client/', include('client_app.urls')),
+    path('API-reservation/', include('reservation_app.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
--- Aeroport_project/reservation_app/urls.py
+++ Aeroport_project/reservation_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ReservationListAPIView, ReservationDetailAPIView
 
-uurlpatterns = [
+urlpatterns = [
     path('reservations/', ReservationListAPIView.as_view(), name='reservation-list'),
     path('reservations/<int:pk>/', ReservationDetailAPIView.as_view(), name='reservation-detail'),
 

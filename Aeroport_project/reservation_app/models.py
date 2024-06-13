b82f19998590dec54fcd1c47415cf0ccf7fc53a2
kalicatt <servaislucas68@gmail.com>
2024-06-13 10:40:51 +0200

    modification logique reservation

--- Aeroport_project/reservation_app/models.py
+++ Aeroport_project/reservation_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Reservation(models.Model):
     client_id = models.ForeignKey('client_app.Clients', on_delete=models.CASCADE)
-    flight_id = models.IntegerField()
+    flight_id = models.ForeignKey('vols_depart_app.FlightDeparture', on_delete=models.CASCADE)
     reservation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

86e50def09408d4cac0b8e9a03327920b30c0470
kalicatt <servaislucas68@gmail.com>
2024-06-13 18:54:12 +0200

    view reservations

--- Aeroport_project/reservation_app/views.py
+++ Aeroport_project/reservation_app/views.py
@@ -1,9 +1,12 @@
+# reservation_app/views.py
+
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Reservation
 from .serializers import ReservationSerializer
+from vols_depart_app.models import FlightDeparture
 
 class ReservationListAPIView(APIView):
     def get(self, request):
@@ -14,8 +17,21 @@ class ReservationListAPIView(APIView):
     def post(self, request):
         serializer = ReservationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            flight_id = serializer.validated_data['flight_id'].id
+            flight = FlightDeparture.objects.get(id=flight_id)
+            
+            # Vérifier s'il y a des sièges disponibles
+            if flight.seats_available > 0:
+                # Créer une nouvelle réservation
+                serializer.save()
+                
+                # Mettre à jour le nombre de sièges disponibles
+                flight.seats_available -= 1
+                flight.save()
+                
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"error": "No seats available"}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ReservationDetailAPIView(APIView):

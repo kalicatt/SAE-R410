f416d23893f13881dc064745a612f3b0ebbee8fc
kalicatt <servaislucas68@gmail.com>
2024-06-13 18:51:04 +0200

    migrations

--- Aeroport_project/vols_depart_app/models.py
+++ Aeroport_project/vols_depart_app/models.py
@@ -7,6 +7,7 @@ class FlightDeparture(models.Model):
     departure_time = models.DateTimeField()
     destination = models.CharField(max_length=100)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
+    sieges_disponible = models.PositiveIntegerField(default=100) 
 
     def __str__(self):
         return self.flight_number

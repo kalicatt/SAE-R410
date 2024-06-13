5a3ebd3f4dd92bb4f2a24e2ccb89c73768868c36
kalicatt <servaislucas68@gmail.com>
2024-06-13 13:01:49 +0200

    migrations

--- Aeroport_project/reservation_app/models.py
+++ Aeroport_project/reservation_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.utils import timezone
 
 class Reservation(models.Model):
     client_id = models.ForeignKey('client_app.Clients', on_delete=models.CASCADE)
     flight_id = models.ForeignKey('vols_depart_app.FlightDeparture', on_delete=models.CASCADE)
     reservation_date = models.DateTimeField(auto_now_add=True)
     prix_ticket = models.DecimalField(max_digits=10, decimal_places=2)
+    
 
     def __str__(self):
         return f"Reservation for flight {self.flight_id}"

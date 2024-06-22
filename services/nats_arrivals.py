a9cfb9c8ffab47efcee5bb921bf4235371bd96d4
Fl0wwDev <mafradin@hotmail.fr>
2024-06-22 18:02:52 +0200

    doc

--- services/nats_arrivals.py
+++ services/nats_arrivals.py
@@ -2,39 +2,52 @@ import json
 import aiohttp
 import logging
 from nats.aio.client import Client as NATS
+import asyncio
 
 # Configuration des logs
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 async def fetch_arrivals():
-    logging.debug("Fetching arrivals from API...")
+    """
+    Récupère les arrivées depuis l'API.
+
+    Returns:
+        dict: Un dictionnaire contenant le statut de l'opération et les données des arrivées.
+    """
+    logging.debug("Récupération des arrivées depuis l'API...")
     async with aiohttp.ClientSession() as session:
         async with session.get('http://127.0.0.1:8002/API-arriver/vol-arriver/') as response:
             if response.status == 200:
                 arrivals = await response.json()
-                logging.debug(f"Fetched arrivals: {arrivals}")
-                return {'status': 'success', 'data': arrivals}
+                logging.debug(f"Arrivées récupérées : {arrivals}")
+                return {'status': 'succès', 'data': arrivals}
             else:
-                logging.error(f"Failed to fetch arrivals, status code: {response.status}")
-                return {'status': 'error', 'message': 'Failed to fetch arrivals'}
+                logging.error(f"Échec de la récupération des arrivées, code de statut : {response.status}")
+                return {'status': 'erreur', 'message': 'Échec de la récupération des arrivées'}
 
 async def run_arrivals():
-    logging.debug("Connecting to NATS server...")
+    """
+    Exécute la récupération des arrivées via NATS.
+    """
+    logging.debug("Connexion au serveur NATS...")
     nc = NATS()
     await nc.connect(servers=["nats://localhost:4222"])
 
-    logging.debug("Connected to NATS server. Subscribing to 'get_arrivals'...")
+    logging.debug("Connecté au serveur NATS. Abonnement à 'get_arrivals'...")
 
     async def message_handler(msg):
         subject = msg.subject
         reply = msg.reply
-        logging.debug(f"Received message on subject '{subject}' with reply '{reply}'")
+        logging.debug(f"Message reçu sur le sujet '{subject}' avec réponse '{reply}'")
 
         if subject == "get_arrivals":
             response = await fetch_arrivals()
             if reply:
                 await nc.publish(reply, json.dumps(response).encode())
-                logging.debug(f"Published response: {response}")
+                logging.debug(f"Réponse publiée : {response}")
 
     await nc.subscribe("get_arrivals", cb=message_handler)
-    logging.debug("Subscribed to 'get_arrivals'")
+    logging.debug("Abonné à 'get_arrivals'")
+
+if __name__ == "__main__":
+    asyncio.run(run_arrivals())
